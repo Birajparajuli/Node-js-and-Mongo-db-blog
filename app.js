a8a162842b54c59f542aa32a1ffbf688f6fa06b8
Biraj <birajparajuli2001@gmail.com>
2021-09-12 15:23:54 +0545

    blog page for each blog added adding and deleting blog from ui

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require("./models/block");
+const { result } = require("lodash");
+const { render } = require("ejs");
 // express app
 const app = express();
 
@@ -18,7 +20,7 @@ mongoose
 app.set("view engine", "ejs");
 
 // app.set('views', 'folder name');  //sets viwews folder to look
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 //milldeware and static files
 app.use(express.static("public"));
@@ -65,6 +67,40 @@ app.get("/blogs", (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
     })
     .catch((err) => {
       console.log(err);
